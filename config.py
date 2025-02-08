ffef1e7f77db9581ce361906b0fbd132575d2669
Oussama Gourari <81024157+oussama-gourari@users.noreply.github.com>
2025-02-08 14:41:05 +0100

    Create config.py

---
+++ config.py
@@ -0,0 +1,19 @@
+"""Configuration variables. Check README.md for details."""
+
+# Reddit credentials and details.
+CLIENT_ID = ""
+CLIENT_SECRET = ""
+REDDIT_USERNAME = ""
+REDDIT_PASSWORD = ""
+
+# Bluesky credentials.
+BSKY_HANDLE = "shareapod.bsky.social"
+BSKY_PASSWORD = ""
+
+# Settings.
+BOT_HOSTER = ""
+SUBREDDIT = "PodcastSharing"
+HASHTAGS = [
+    "ShareAPodcast",
+]
+CATCHUP_LIMIT = 0
