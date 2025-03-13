2b9d171793dc16b42bfb61aece1e08902791f13c
Oussama Gourari <81024157+oussama-gourari@users.noreply.github.com>
2025-03-13 15:45:30 +0100

    update

--- scripts/Run.sh
+++ scripts/Run.sh
@@ -1,4 +1,4 @@
-if [ $(pgrep -cf Run.sh) -gt 0 ]; then
+if [ $(pgrep -cf skycast.py) -gt 0 ]; then
     tmux new -As Skycast
 else
     tmux new -As Skycast \; send-keys "uv run --with \"prawcore>=3.0.1\" skycast.py" Enter
