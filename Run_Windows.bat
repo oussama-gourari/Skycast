2b8ca708ae1e7e182a46eda199db6c51fdc1a929
Oussama Gourari <81024157+oussama-gourari@users.noreply.github.com>
2025-03-14 15:22:19 +0100

    auto install uv if not found

--- Run_Windows.bat
+++ Run_Windows.bat
@@ -1,4 +1,8 @@
 @echo off
+where /Q uv
+if NOT %errorlevel% == 0 (
+    powershell -ExecutionPolicy ByPass -c "irm https://astral.sh/uv/install.ps1 | iex"
+)
 cls
 title Skycast
 uv run --with prawcore^>=3.0.1 src/skycast.py
